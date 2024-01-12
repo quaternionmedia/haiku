c00b6c8b67bf7c49ec8ba84cfb7083d4cc4660b2
Harpo <harpo@harpo.me>
2024-01-11 17:17:04 -0800

    Adds @actions/github to dependencies

--- haiku.js
+++ haiku.js
@@ -1,3 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const token = core.getInput('token');
+const octokit = github.getOctokit(token);
+const context = github.context;
+
 const issue = context.issue;
 const issueBody = context.payload.issue.body;
 const syllable = require('syllable');
